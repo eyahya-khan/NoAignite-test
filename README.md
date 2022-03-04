74ef98312abb8216842ca9b5992d679a86c4a573
eyahya-khan <eyahyakhan@gmail.com>
2022-03-04 15:55:07 +0100

    Responsive

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-## NoAignite test
+# NoAignite test
 
-# Requirement:
+## Requirement:
 
 1. Use React
 2. API
 3. Styled component
 4. Loading indication
+
+# View on live
 
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,51 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { BeatLoader } from "react-spinners";
+import { device } from "./device";
 
 const Display = styled.div`
   font-family: sans-serif;
   margin-top: 50px;
-  font-size: 16px;
+  padding: 25px
+  font-size: 20px;
   text-align: center;
 `;
 
 const DisplayInline = styled.div`
-display: grid;
-grid-template-columns: repeat(3, 1fr);
-grid-template-rows: 1fr;
+  display: flex;
+  flex-direction: column;
 
-max-width: 40%;
-margin-left: 30%;
+  margin: 70px 0px;
+
+  @media ${device.tablet} {
+    max-width: 80%;
+    margin-left: 10%;
+    margin-top: 90px;
+
+
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  @media ${device.laptop} {
+    max-width: 60%;
+    margin-left: 20%;
+    margin-top: 15%;
+  }
+
+  @media ${device.laptopL} {
+    max-width: 50%;
+    margin-left: 25%;
+    margin-top: 15%;
+  }
+
+  @media ${device.desktop} {
+    max-width: 30%;
+    margin-left: 35%;
+    margin-top: 15%;
+  }
 `;
-
 
 const ButtonIncrement = styled.button`
   font-family: sans-serif;
@@ -48,80 +76,106 @@ const ButtonDecrement = styled.button`
   color: #000000;
   &:hover {
     background: black;
-    color:white
+    color: white;
   }
 `;
 
 const Title = styled.p`
-  font-family: sans-serif;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  font-size: 18px;
+  display:flex;
+  flex-direction:column;
+  justify-content:flext-start;
+
+  padding:10px;
+  font-size: 20px;
   font-weight: 200;
   text-align: center;
   background-color: #ffffff;
 `;
 
+const Span = styled.span`
+  padding:10px;
+  line-height:1.5em;
+
+`;
 
 function Counter() {
-//for fetching data
-const [post, setPost] = useState({});
-//for counter
+  //loading icon
+  const [isLoading, setIsLoading] = useState(false);
+  //for fetching data
+  const [post, setPost] = useState({});
+  const [error, setError] = useState("");
+  //for counter
   const initialCount = 0;
   const [count, setCount] = useState(initialCount);
 
   const repoList = [
-    'eslint/eslint',
-    'oakwood/front-end-questions',
-    'babel/babel',
-    'webpack/webpack',
-    'storybooks/storybook',
-    'facebook/react',
-    'reactjs/redux',
-    'expressjs/express'
-  ]
+    "eslint/eslint",
+    "oakwood/front-end-questions",
+    "babel/babel",
+    "webpack/webpack",
+    "storybooks/storybook",
+    "facebook/react",
+    "reactjs/redux",
+    "expressjs/express",
+  ];
 
-const increment = ()=>{ 
-  setCount((count + 1) > repoList.length-1 ? count : count + 1)
-}
-const decrement = ()=>{ 
-  setCount((count - 1) < 0 ? count : count - 1)
-}
+  const increment = () => {
+    setCount(count + 1 > repoList.length - 1 ? count : count + 1);
+    //loading
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 1000);
+  };
+  const decrement = () => {
+    setCount(count - 1 < 0 ? count : count - 1);
+    //loading
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 1000);
+  };
 
-useEffect(() => {
-  axios
-    .get(`https://api.github.com/repos/${repoList[count]}`)
-    .then((res) => {
-      console.log(res);
-      setPost(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}, [count]);
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/repos/${repoList[count]}`)
+      .then((res) => {
+        setPost(res.data);
+      })
+      .catch((err) => {
+        setPost(err.message);
+        setError(err.message);
+      });
+  }, [count]);
 
   return (
     <Display>
       <DisplayInline>
-          <ButtonDecrement primary onClick={decrement}>- DECREMENT </ButtonDecrement>
-          <Title>Count: {count}</Title>
-          <ButtonIncrement primary onClick={increment}>+ INCREMENT</ButtonIncrement>
+        <ButtonDecrement primary onClick={decrement}>
+          - DECREMENT
+        </ButtonDecrement>
+
+        <Title>Count: {count}</Title>
+
+        <ButtonIncrement primary onClick={increment}>
+          <Span>+</Span> INCREMENT
+        </ButtonIncrement>
       </DisplayInline>
+
+      {isLoading && <BeatLoader color="green" loading />}
+
+      {error === post ? error : null}
+
       <Title>
+      <Span>
         <strong>Title: </strong>
         {post.full_name}
-      </Title>
-
-      <Title>
+        </Span>
+          <Span>
         <strong>Description: </strong>
         {post.description}
-      </Title>
-
-      <Title>
+        </Span>  
+      <Span>
         <strong>Amounts of stars: </strong>
         {post.stargazers_count}
+      </Span>
       </Title>
-
     </Display>
   );
 }
